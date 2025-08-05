d75dd2128500edef804eab72695289d60987b01f
miao-pqz <1125583676.qq.com>
2025-08-05 10:51:15 +0800

    01: setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,15 @@
 import { add } from "@workspace/math/add";
+import { Input } from "@workspace/ui/components/input";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
+        <h1 className="text-2xl font-bold">Hello World/widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(1, 2)}</p>
+        <p>{add(2, 2)}</p>
+        <Input type="text" placeholder="Enter your name" />
       </div>
     </div>
   )
