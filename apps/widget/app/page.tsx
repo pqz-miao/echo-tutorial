46819ed955c39b7b344605d2ec1b03eb3cd9d1ff
miao-pqz <1125583676.qq.com>
2025-08-05 16:55:49 +0800

    02: convex package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,16 +1,20 @@
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/widget</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 2)}</p>
-        <Input type="text" placeholder="Enter your name" />
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <Button onClick={() => addUser()}>Add John Doe</Button>
+      <div className="max-w-lg w-full mx-auto">
+        {JSON.stringify(users, null, 2)}
       </div>
     </div>
-  )
+  );
 }
