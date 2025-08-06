13ca45d1588d1bef6565b8ff2fe85fc3280c577f
miao-pqz <1125583676.qq.com>
2025-08-06 20:14:45 +0800

    03: clerk authentication

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
