8ea70f296dd78959a17460084cecd01376dfeaa7
miao-pqz <1125583676.qq.com>
2025-08-14 08:42:46 +0800

    11: widget screen router

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { WidgetFooter } from "../components/widget-footer";
+import { useAtomValue } from "jotai";
+
+import { screenAtom } from "../../atoms/widget-atoms";
 import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
@@ -8,11 +10,23 @@ interface Props {
 };
 
 export const WidgetView = ({ organizationId }: Props) => {
+    const screen = useAtomValue(screenAtom);
+
+    const screenComponents = {
+        error: <p>TODO: Error screen</p>,
+        loading: <p>TODO: Loading screen</p>,
+        auth: <WidgetAuthScreen />,
+        voice: <p>TODO: Voice screen</p>,
+        inbox: <p>TODO: Inbox screen</p>,
+        selection: <p>TODO: Selection screen</p>,
+        chat: <p>TODO: Chat screen</p>,
+        contact: <p>TODO: Contact screen</p>,
+    };
+
     return (
         // TODO: Confirm whether the min-h-screen & min-w-screen are needed
         <main className="min-h-screen min-w-screen flex flex-col h-full w-full overflow-hidden rounded-xl border bg-muted">
-            <WidgetAuthScreen />
-            {/* <WidgetFooter /> */}
+            {screenComponents[screen]}
         </main>
     );
 };
