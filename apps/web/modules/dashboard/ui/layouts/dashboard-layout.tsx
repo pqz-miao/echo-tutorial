a51dd73897241027b3f16e2fa99f7685a94cba0d
miao-pqz <1125583676.qq.com>
2025-08-20 10:44:05 +0800

    18: dashboard chat

--- apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "jotai";
 import { cookies } from "next/headers";
 
 import { SidebarProvider } from "@workspace/ui/components/sidebar";
@@ -8,17 +9,20 @@ import { DashboardSidebar } from "../components/dashboard-sidebar";
 
 export const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     const cookieStore = await cookies();
+    // Using SIDEBAR_COOKIE_NAME from sidebar component does not work due to  monorepo and SSR
     const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
     
     return (
         <AuthGuard>
             <OrganizationGuard>
-                <SidebarProvider defaultOpen={defaultOpen}>
-                    <DashboardSidebar />
-                    <main className="flex flex-1 flex-col">
-                        {children}
-                    </main>
-                </SidebarProvider>
+                <Provider>
+                    <SidebarProvider defaultOpen={defaultOpen}>
+                        <DashboardSidebar />
+                        <main className="flex flex-1 flex-col">
+                            {children}
+                        </main>
+                    </SidebarProvider>
+                </Provider>
             </OrganizationGuard>
         </AuthGuard>
     );
